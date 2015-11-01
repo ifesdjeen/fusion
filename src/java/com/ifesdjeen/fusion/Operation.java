02d9cba8f44423398d94cfd77602bd0add38bc38
Alex Petrov <oleksandr.petrov@gmail.com>
2015-11-01 22:55:54 +0100

    Rewrite ops

--- src/java/com/ifesdjeen/fusion/Operation.java
+++ src/java/com/ifesdjeen/fusion/Operation.java
@@ -2,93 +2,74 @@ package com.ifesdjeen.fusion;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public abstract class Operation<T, D> implements Stream<D> {
+public interface Operation<T, D> extends Stream<D> {
 
-  protected final Stream<T> parent;
+  //  protected final Stream<T> parent;
+  //
+  //  public Operation(Stream<T> stream) {
+  //    this.parent = stream;
+  //  }
 
-  public Operation(Stream<T> stream) {
-    this.parent = stream;
-  }
-
-
-
-  protected abstract Step<D> step(Step.Done<T> done);
-
-  protected abstract Step<D> step(Step.Skip<T> skip);
-
-  protected abstract Step<D> step(Step.Yield<T> yield);
-
-  public Step<D> next() {
-    Step<T> prev = parent.next();
-    if (prev instanceof Step.Done) {
-      return step((Step.Done<T>) prev);
-    } else if (prev instanceof Step.Yield) {
-      return step((Step.Yield<T>) prev);
-    } else {
-      return step((Step.Skip<T>) prev);
-    }
-  }
 
   public static <FROM, TO> Operation<FROM, TO> map(Function<FROM, TO> fn, Stream<FROM> stream) {
-    return new Operation<FROM, TO>(stream) {
-      @Override
-      public Step<TO> step(Step.Done<FROM> done) {
-        return new Step.Done<>();
+    return () -> {
+      Step step = stream.next();
+      if (step.getState() == State.YIELD) {
+        step.setItem(fn.apply(step.getItem()));
       }
-
-      @Override
-      protected Step<TO> step(Step.Skip<FROM> skip) {
-        return new Step.Skip<>(this);
-      }
-
-      @Override
-      public Step<TO> step(Step.Yield<FROM> yield) {
-        return new Step.Yield<>(this, fn.apply(yield.getItem()));
-      }
+      return step;
     };
+
   }
 
   public static <T> Operation<T, T> filter(Predicate<T> pred, Stream<T> stream) {
-    return new Operation<T, T>(stream) {
-
-      @Override
-      protected Step<T> step(Step.Done<T> done) {
-        return done;
+    return () -> {
+      Step step = stream.next();
+      if (step.getState() == State.YIELD && pred.test(step.getItem())) {
+        return new Step(State.SKIP, null);
       }
-
-      @Override
-      protected Step<T> step(Step.Skip<T> skip) {
-        return skip;
-      }
-
-      @Override
-      protected Step<T> step(Step.Yield<T> yield) {
-        T item = yield.getItem();
-        if (pred.test(item)) {
-          return new Step.Yield<>(this, item);
-        } else {
-          return new Step.Skip<>(this);
-        }
-
-      }
+      return step;
     };
+  }
+
+  public static <ACC, T> ACC fold(Stream<T> stream,
+                                  ACC init_,
+                                  BiFunction<ACC, T, ACC> foldf) {
+    ACC init = init_;
+
+    Step s = stream.next();
+
+    while (true) {
+      if (s.getState() == State.YIELD) {
+        init = foldf.apply(init, s.getItem());
+      } else if (s.getState() == State.DONE) {
+        break;
+      }
+
+      s = stream.next();
+    }
+
+    return init;
   }
 
   public static <T> List<T> toList(Stream<T> stream) {
     List<T> list = new ArrayList<>();
 
-    Step<T> s = stream.next();
+    Step s = stream.next();
 
-    while(!(s instanceof Step.Done)) {
-      if (s instanceof Step.Yield) {
-        list.add(((Step.Yield<T>) s).getItem());
+    while (s.getState() != State.DONE) {
+      if (s.getState() == State.YIELD) {
+        list.add(s.getItem());
       }
       s = stream.next();
     }
 
     return list;
   }
+
+
 }
