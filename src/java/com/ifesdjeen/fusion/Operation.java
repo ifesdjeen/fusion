320c865560293f5772003a6bc8f4f347319ec866
Alex Petrov <oleksandr.petrov@gmail.com>
2015-11-02 21:18:57 +0100

    Improve the way the streams are fused, use bottom-top fusion instead of breaking from above

--- src/java/com/ifesdjeen/fusion/Operation.java
+++ src/java/com/ifesdjeen/fusion/Operation.java
@@ -1,75 +1,43 @@
 package com.ifesdjeen.fusion;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public interface Operation<T, D> extends Stream<D> {
+public interface Operation<FROM, TO> extends Consumer<FROM> {
 
-  //  protected final Stream<T> parent;
-  //
-  //  public Operation(Stream<T> stream) {
-  //    this.parent = stream;
-  //  }
-
-
-  public static <FROM, TO> Operation<FROM, TO> map(Function<FROM, TO> fn, Stream<FROM> stream) {
-    return () -> {
-      Step step = stream.next();
-      if (step.getState() == State.YIELD) {
-        step.setItem(fn.apply(step.getItem()));
-      }
-      return step;
-    };
-
-  }
-
-  public static <T> Operation<T, T> filter(Predicate<T> pred, Stream<T> stream) {
-    return () -> {
-      Step step = stream.next();
-      if (step.getState() == State.YIELD && pred.test(step.getItem())) {
-        return new Step(State.SKIP, null);
-      }
-      return step;
+  public static <FROM, TO> Operation<FROM, TO> map(Function<FROM, TO> fn,
+                                                   Consumer<TO> downstream) {
+    return (a) -> {
+      downstream.accept(fn.apply(a));
     };
   }
 
-  public static <ACC, T> ACC fold(Stream<T> stream,
-                                  ACC init_,
-                                  BiFunction<ACC, T, ACC> foldf) {
-    ACC init = init_;
-
-    Step s = stream.next();
-
-    while (true) {
-      if (s.getState() == State.YIELD) {
-        init = foldf.apply(init, s.getItem());
-      } else if (s.getState() == State.DONE) {
-        break;
+  public static <T> Operation<T, T> filter(Predicate<T> pred,
+                                           Consumer<T> downstream) {
+    return (a) -> {
+      if (pred.test(a)) {
+        downstream.accept(a);
       }
-
-      s = stream.next();
-    }
-
-    return init;
+    };
   }
 
-  public static <T> List<T> toList(Stream<T> stream) {
-    List<T> list = new ArrayList<>();
+  public static <ACC, T> Consumer<T> fold(AtomicReference<ACC> init,
+                                          BiFunction<ACC, T, ACC> foldf) {
+    return (a) -> {
+      ACC d = init.updateAndGet((current) -> foldf.apply(current, a));
+    };
 
-    Step s = stream.next();
 
-    while (s.getState() != State.DONE) {
-      if (s.getState() == State.YIELD) {
-        list.add(s.getItem());
-      }
-      s = stream.next();
-    }
-
-    return list;
   }
 
+  public static <T> Consumer<T> toList(List<T> list) {
+    return (a) -> {
+      list.add(a);
+    };
+  }
 
 }
--- src/java/com/ifesdjeen/fusion/Stream.java
+++ src/java/com/ifesdjeen/fusion/Stream.java
@@ -1,30 +1,16 @@
 package com.ifesdjeen.fusion;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public interface Stream<T> {
 
-  public Step next();
+  //  public Step next();
 
-  public static <T> Stream<T> fromList(List<T> input) {
-    return new Stream<T>() {
-
-      private int pointer = 0;
-
-      private Step step = new Step(State.YIELD, null);
-      @Override
-      public Step next() {
-        if (pointer < input.size()) {
-          step.setItem(input.get(pointer++));
-          step.setState(State.YIELD);
-        } else {
-          step.setItem(null);
-          step.setState(State.DONE);
-        }
-        return step;
-      }
-
-    };
+  public static <T> void fromList(List<T> input, Consumer<T> consumer) {
+    for (T i : input) {
+      consumer.accept(i);
+    }
   }
 
 
