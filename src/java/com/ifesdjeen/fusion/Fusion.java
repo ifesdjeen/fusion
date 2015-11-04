870302e13f280caccbb8d7f3c8a9715df0814f20
Alex Petrov <oleksandr.petrov@gmail.com>
2015-11-04 19:49:40 +0100

    Add break to fusion

--- src/java/com/ifesdjeen/fusion/Fusion.java
+++ src/java/com/ifesdjeen/fusion/Fusion.java
@@ -5,6 +5,8 @@ import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -14,23 +16,46 @@ import java.util.function.Predicate;
 public class Fusion<INIT, FROM> {
 
   private final List<Function<Consumer, Consumer>> suppliers;
+  private final AtomicBoolean                      doBreak;
 
   public Fusion() {
-    this(new ArrayList<>());
+    this(new ArrayList<>(),
+         new AtomicBoolean());
   }
 
-  protected Fusion(List<Function<Consumer, Consumer>> suppliers) {
+  protected Fusion(List<Function<Consumer, Consumer>> suppliers,
+                   AtomicBoolean doBreak) {
     this.suppliers = suppliers;
+    this.doBreak = doBreak;
   }
 
   public <TO> Fusion<INIT, TO> map(Function<FROM, TO> fn) {
     suppliers.add((downstream) -> Operation.map(fn, downstream));
-    return new Fusion<>(suppliers);
+    return downstream();
   }
 
   public Fusion<INIT, FROM> filter(Predicate<FROM> pred) {
     suppliers.add((downstream) -> Operation.filter(pred, downstream));
-    return new Fusion<>(suppliers);
+    return downstream();
+  }
+
+  public Fusion<INIT, FROM> take(int howMany) {
+    suppliers.add((downstream) -> {
+      AtomicInteger countdown = new AtomicInteger(howMany);
+      return (i) -> {
+        downstream.accept(i);
+        int more = countdown.decrementAndGet();
+        if (more == 0) {
+          doBreak.set(true);
+        }
+      };
+
+    });
+    return downstream();
+  }
+
+  protected <TO> Fusion<INIT, TO> downstream() {
+    return new Fusion<INIT, TO>(suppliers, doBreak);
   }
 
   public Consumer<INIT> end(AtomicReference<FROM> init) {
@@ -78,8 +103,11 @@ public class Fusion<INIT, FROM> {
       stack = gen.apply(stack);
     }
 
-    Consumer<INIT> finalized = (Consumer<INIT>)stack;
+    Consumer<INIT> finalized = (Consumer<INIT>) stack;
     while (iterator.hasNext()) {
+      if (doBreak.get()) {
+        break;
+      }
       finalized.accept(iterator.next());
     }
     return finalizer.get();
